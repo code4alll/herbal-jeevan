1ed4a317468bbfb373e40e31a1708696dcc08dde
code4alll <aditiranjan096@gmail.com>
2024-12-04 12:02:27 +0530

    cros config

--- src/main/java/com/ecommerce/HerbalJeevan/Controller/AdminController.java
+++ src/main/java/com/ecommerce/HerbalJeevan/Controller/AdminController.java
@@ -3,6 +3,7 @@ package com.ecommerce.HerbalJeevan.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.ecommerce.HerbalJeevan.Utility.Response;
 
 @RestController
 @RequestMapping("/api/admin")
+@CrossOrigin(origins = "*")
 public class AdminController {
 	@Autowired
 	private UserService userService;
--- src/main/java/com/ecommerce/HerbalJeevan/Controller/UserController.java
+++ src/main/java/com/ecommerce/HerbalJeevan/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.ecommerce.HerbalJeevan.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.ecommerce.HerbalJeevan.services.email.OTPservices;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UserController {
 	
 	@Autowired
